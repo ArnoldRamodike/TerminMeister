09687c616153a85231d6632f23660e8b8f32f59b
ArnoldRamodike <riccoarnold23@gmail.com>
2024-05-14 16:36:38 +0200

    Displaying current slected search parsms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from "@/providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModel from "./components/modals/RentModel";
+import SearchModal from "./components/modals/SearchModal";
 
 
 const nunito = Nunito({ subsets: ["latin"] });
@@ -29,6 +30,7 @@ export default async function RootLayout({
         <RegisterModal/>
         <LoginModal/>
         <RentModel/>
+        <SearchModal/>
         <Navbar currentUser={currentUser} />
         <div className="pb-20 pt-28">
           {children}
